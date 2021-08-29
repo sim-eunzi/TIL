ee72de95539a2b12687064590fd1806c2de4c3e4
sim-eunzi <kokopink12@soongsil.ac.kr>
2021-08-30 01:33:40 +0900

    상태를 갱신하기- 이벤트 처리

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 class App extends React.Component {
+  constructor() {
+    super() 
+
+    this.state = {
+      searchKeyword: ''
+    }
+  }
+
+  handleChangeInput(event) {
+    this.setState({
+      searchKeyword: event.target.value
+    })
+
+    // this.state.searchKeyword = event.target.value
+    // this.forceUpdate()
+  }
+
   render() {
     return (
       <>
@@ -7,7 +24,13 @@ class App extends React.Component {
         </header>
         <div className="container">
           <form>
-            <input type="text" placeholder="검색어를 입력하세요" autoFocus />
+            <input 
+              value={this.state.searchKeyword} 
+              type="text" 
+              placeholder="검색어를 입력하세요" 
+              autoFocus 
+              onChange={e => this.handleChangeInput(e)}
+            />
             <button type="reset" className="btn-reset"></button>
           </form>
         </div>
