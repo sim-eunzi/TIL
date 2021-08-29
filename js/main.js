483f6778c1bba204301767d9eaad936748d7266b
sim-eunzi <kokopink12@soongsil.ac.kr>
2021-08-30 01:23:46 +0900

    React component 변경

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
-const element = (
-  <>
-    <header>
-      <h2 className="container"> 검색 </h2>
-    </header>
-    <div className="container">
-      <form>
-        <input type="text" placeholder="검색어를 입력하세요" autoFocus />
-        <button type="reset" className="btn-reset"></button>
-      </form>
-    </div>
-  </>
-)
-ReactDOM.render(element, document.querySelector("#app"))
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h2 className="container"> 검색 </h2>
+        </header>
+        <div className="container">
+          <form>
+            <input type="text" placeholder="검색어를 입력하세요" autoFocus />
+            <button type="reset" className="btn-reset"></button>
+          </form>
+        </div>
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"))
